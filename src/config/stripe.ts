a790c46c01a0ec37aaab4bbb1be5f1f75ca918c7
Arpan Biswas <talk2arpan@protonmail.com>
2024-01-18 11:19:16 +0530

    feat: initial commit

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+    {
+        name: "Free",
+        slug: "free",
+        quota: 10,
+        pagesPerPdf: 5,
+        price: {
+            amount: 0,
+            priceIds: {
+                test: '',
+                production: ''
+            }
+        }
+    },
+    {
+        name: "Pro",
+        slug: "pro",
+        quota: 50,
+        pagesPerPdf: 25,
+        price: {
+            amount: 190,
+            priceIds: {
+                test: 'price_1OZYWySGZ7Iy1SSaRhm1T63s',
+                production: ''
+            }
+        }
+    },
+]
